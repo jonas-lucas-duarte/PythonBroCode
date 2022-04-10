687a72c1fac07a51c125361bd5904f779fa0d6dd
jonas-lucas-duarte <jonaslucasduarte.github@gmail.com>
2022-04-10 17:43:33 -0300

    New File 002-Variables/main.py

---
+++ 002-Variables/main.py
@@ -0,0 +1,21 @@
+# variable = a container for a value, Behaves as the value that it contains
+
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name + " " + last_name
+
+print("Hello " + full_name)
+#print(type(name))
+
+age = 21
+age += 1
+print("Your age is: " + str(age))
+#print(type(age))
+
+height = 250.5
+print("Your height is: " + str(height) + "cm")
+#print(type(height))
+
+human = True
+print("Are you a human: " + str(human))
+#print(type(human))
