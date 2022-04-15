fb5b3c11cc67551c4086008faedd2c1ddf947f00
jonas-lucas-duarte <jonaslucasduarte.github@gmail.com>
2022-04-15 17:48:41 -0300

    New File 003-MultipleAssignment/main.py

---
+++ 003-MultipleAssignment/main.py
@@ -0,0 +1,18 @@
+# multiple assignment = allows us to assign multiple variables at the same time in one line of code
+
+#name = "Bro"
+#age = 21
+#attractive = True
+
+name, age, attractive = "Bro", 21, True
+
+print(name)
+print(age)
+print(attractive)
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
