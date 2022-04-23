3304078178d3740d712b44194a4f87b1ac94750f
jonas-lucas-duarte <jonaslucasduarte.github@gmail.com>
2022-04-23 19:50:03 -0300

    New File 004-StringMethods/main.py

---
+++ 004-StringMethods/main.py
@@ -0,0 +1,12 @@
+name = "Bro Code"
+
+print(len(name))
+print(name.find("o"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace("o", "a"))
+print(name*3)
